e5809054f8c637644701883fcc57345f55db31af
Napoleon-WONG559 <54040022+Napoleon-WONG559@users.noreply.github.com>
2023-03-02 16:26:13 +0800

    Create data_extractor.py

---
+++ data_extractor.py
@@ -0,0 +1,15 @@
+import csv
+
+class DataExtractor(object):
+    def __init__(self,
+                 filename):
+        record=[]
+        with open(filename,newline='') as csvfile:
+            read=csv.reader(csvfile)
+            count=0
+            for item in read:
+                record.append(item)
+                count+=1
+                if(count==6836):
+                    break
+        self.data=record
